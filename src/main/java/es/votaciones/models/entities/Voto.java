0649a0f592237f2158a1b1bd5f5110853d7bab39
bl0819 <mabl0819@192.168.7.31>
2015-03-04 14:56:51 +0100

    ADD classVoto

---
+++ src/main/java/es/votaciones/models/entities/Voto.java
@@ -0,0 +1,10 @@
+package es.votaciones.models.entities;
+
+import java.io.Serializable;
+
+public class Voto implements Serializable {
+	public static final String TABLE = "voto";
+    
+	public static final String ID="ID";
+	private Integer id;
+}
