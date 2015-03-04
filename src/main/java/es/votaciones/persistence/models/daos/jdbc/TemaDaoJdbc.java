93e27688b85aed54acf041dacd48179c8ee805d6
magacoelho <mgacoelho@yahoo.es>
2015-03-04 23:29:36 +0100

    UPDATE class TemaDaoJdbc

--- src/main/java/es/votaciones/persistence/models/daos/jdbc/TemaDaoJdbc.java
+++ src/main/java/es/votaciones/persistence/models/daos/jdbc/TemaDaoJdbc.java
@@ -2,11 +2,16 @@ package es.votaciones.persistence.models.daos.jdbc;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
 import es.votaciones.persistence.models.daos.TemaDao;
 import es.votaciones.persistence.models.entities.Tema;
 
 public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements TemaDao {
-
+	 private Logger log = LogManager.getLogger(TemaDaoJdbc.class);
 	@Override
 	public void create(Tema entity) {
 		// TODO Auto-generated method stub
@@ -36,10 +41,12 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements TemaDa
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	private static final String SQL_CREATE_TABLE ="CREATE TABLE %s (%s INT NOT NULL AUTO_INCREMENT, %s VARCHAR(255), "
+            + "%s VARCHAR(255)";
+	
 	public static String sqlToCreateTable() {
-		// TODO Auto-generated method stub
-		return null;
+			return String.format(SQL_CREATE_TABLE, Tema.TABLE, Tema.ID, Tema.DESCRIPCION, Tema.PREGUNTA );
 	}
 
 }
