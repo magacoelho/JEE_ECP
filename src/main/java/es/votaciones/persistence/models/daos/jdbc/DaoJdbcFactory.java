65bf417372f5672f956216b6b3ff4861b121ccc7
magacoelho <mgacoelho@yahoo.es>
2015-03-04 20:48:16 +0100

    ADD clase GenericDaoJDBC - DaoJdbcFactory

---
+++ src/main/java/es/votaciones/persistence/models/daos/jdbc/DaoJdbcFactory.java
@@ -0,0 +1,10 @@
+package es.votaciones.persistence.models.daos.jdbc;
+
+public class DaoJdbcFactory {
+
+	public static Object getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
