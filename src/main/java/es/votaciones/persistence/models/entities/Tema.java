1ae793d1abdee63d93c84e00472d0f3055e63013
bl0819 <mabl0819@192.168.7.31>
2015-03-04 15:21:07 +0100

    Update clase Tema

--- src/main/java/es/votaciones/persistence/models/entities/Tema.java
+++ src/main/java/es/votaciones/persistence/models/entities/Tema.java
@@ -1,5 +1,104 @@
 package es.votaciones.persistence.models.entities;
 
+import java.util.List;
+
 public class Tema {
+	
+	private Integer id;
+	private String  descripcion;
+	private String pregunta;
+	private List<Voto> votos;
+	
+	public Tema() {
+	
+	}
+
+	public Tema(String descripcion, String pregunta) {
+		super();
+		this.descripcion = descripcion;
+		this.pregunta = pregunta;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+
+	public List<Voto> getVotos() {
+		return votos;
+	}
+
+	public void setVotos(List<Voto> votos) {
+		this.votos = votos;
+	}
+
+	@Override
+	public String toString() {
+		return "Tema [id=" + id + ", descripcion=" + descripcion
+				+ ", pregunta=" + pregunta + ", votos=" + votos + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((pregunta == null) ? 0 : pregunta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tema other = (Tema) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (pregunta == null) {
+			if (other.pregunta != null)
+				return false;
+		} else if (!pregunta.equals(other.pregunta))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	
 
 }
