3ae74b8afcb6d8ed51979fdb147219d02b1deea5
bl0819 <mabl0819@192.168.7.31>
2015-03-05 15:00:35 +0100

    ADD clase test TemaDaoJpaTestData

---
+++ src/test/java/es/votaciones/persistence/models/daos/jpa/data/TemaDaoJpaTestData.java
@@ -0,0 +1,15 @@
+package es.votaciones.persistence.models.daos.jpa.data;
+
+public class TemaDaoJpaTestData {
+	
+	
+	public static final String [][] TEMAS ={{"DEPORTES","Actuación Michael Phelps en los Olimpicos 2002"}
+	   , {"COCINA","Receta Tortilla de patata con morcillas de Burgos"},{"OCIO","Limpieza de las playas en Alicante"}
+		,{"CIENCIA","Impacto del Bioelementos en el ecosistema"}};
+
+    public TemaDaoJpaTestData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
