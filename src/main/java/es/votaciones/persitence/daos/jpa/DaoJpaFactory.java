73e441fee19b6f6fcf6c39747f6b9ea1114bd6fc
bl0819 <mabl0819@192.168.7.31>
2015-03-05 13:07:38 +0100

    add copiar y adaptar calse DaoJpaFactory

---
+++ src/main/java/es/votaciones/persitence/daos/jpa/DaoJpaFactory.java
@@ -0,0 +1,56 @@
+package es.votaciones.persitence.daos.jpa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
+
+import es.votaciones.persistence.models.daos.DaoFactory;
+import es.votaciones.persistence.models.daos.TemaDao;
+import es.votaciones.persistence.models.daos.VotoDao;
+
+
+
+public class DaoJpaFactory extends DaoFactory {
+    private static final String PERSISTENCE_UNIT = "tictactoe";
+
+    private static EntityManagerFactory entityManagerFactory; 
+
+    public DaoJpaFactory() {
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+        }
+        return entityManagerFactory;
+    }
+
+    public static void dropAndCreateTables() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+                PersistenceUnitProperties.DROP_AND_CREATE);
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+	@Override
+	public TemaDao getTemaDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VotoDao getVotoDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+   
+
+}
