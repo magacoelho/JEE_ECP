e7ac406a6aecdf2a3798156f9f02f88ad20fbe67
magacoelho <mgacoelho@yahoo.es>
2015-03-07 18:53:25 +0100

    Add TemaDaoJpaTest.init y atributos

--- src/test/java/es/votaciones/persistence/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/votaciones/persistence/models/daos/jpa/TemaDaoJpaTest.java
@@ -1,5 +1,23 @@
 package es.votaciones.persistence.models.daos.jpa;
 
+import org.junit.Before;
+
+import es.votaciones.persistence.models.daos.TemaDao;
+import es.votaciones.persistence.models.daos.jpa.data.TemaDaoJpaTestData;
+
+
+
 public class TemaDaoJpaTest {
+	
+	private TemaDao dao= DaoJpaFactory.getFactory().getTemaDao();
+	private TemaDaoJpaTestData data;
+	
+	@Before
+	public void init(){
+		data= new TemaDaoJpaTestData();
+	  // aqui tendria que meter lo de borrar las tablas...
+	
+	}
+	
 
 }
