b98d9851a98e8537c1f2b2b922f7862be30eda7c
magacoelho <mgacoelho@yahoo.es>
2015-03-07 18:21:56 +0100

    Upadte class ArrayToListTransformer.transform(String[][] arrayAtributos)

--- src/test/java/es/votaciones/utils/ArrayToListTransformer.java
+++ src/test/java/es/votaciones/utils/ArrayToListTransformer.java
@@ -1,5 +1,23 @@
 package es.votaciones.utils;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import es.votaciones.persistence.models.entities.Tema;
+
 public class ArrayToListTransformer {
+	
+	public static Iterator<Tema> transform(String[][] arrayAtributos) {
+        List<Tema> result = new ArrayList<Tema>();
+        for (int i = 0; i < arrayAtributos.length; i++) {
+        	Tema tema = null;
+        	if(arrayAtributos!=null){
+        		tema= new Tema(arrayAtributos[i][0],arrayAtributos[i][1] );
+        	}
+            result.add(tema);
+        }
+        return result.iterator();
+    }
 
 }
