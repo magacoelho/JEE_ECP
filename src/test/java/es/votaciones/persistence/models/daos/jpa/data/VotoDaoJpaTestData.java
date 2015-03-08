929f05ac63092b8bf1ebf380d1ec4e910a40514d
magacoelho <mgacoelho@yahoo.es>
2015-03-08 23:44:34 +0100

    UPDATE VotoDaoJpaTestData atrributo VOTOS

--- src/test/java/es/votaciones/persistence/models/daos/jpa/data/VotoDaoJpaTestData.java
+++ src/test/java/es/votaciones/persistence/models/daos/jpa/data/VotoDaoJpaTestData.java
@@ -3,7 +3,13 @@ package es.votaciones.persistence.models.daos.jpa.data;
 import es.votaciones.persistence.models.entities.Voto;
 
 public class VotoDaoJpaTestData {
-
+	public static final String [][] VOTOS ={{"8","192.123.4.5" , "NivelEstudio.PRIMARIA"},
+											{"5","192.123.4.5" , "NivelEstudio.BACHILLERATO"},
+											{"1","192.123.4.5" , "NivelEstudio.PRIMARIA"},
+											{"10","192.123.4.5" , "NivelEstudio.PRIMARIA"},
+											{"3","192.123.4.5" , "NivelEstudio.BACHILLERATO"},
+											{"6","192.123.4.5" , "NivelEstudio.UNIVERSIDAD"}};
+											
 	public boolean hasNextVoto() {
 		// TODO Auto-generated method stub
 		return false;
