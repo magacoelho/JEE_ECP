5f1af5988f0cb8cb12774c592c8c0ef38df5ba91
magacoelho <mgacoelho@yahoo.es>
2015-03-08 23:10:57 +0100

    UPDATE VotoDaoJpaTest.init ADD VotoDaoJpaTestData

--- src/test/java/es/votaciones/persistence/models/daos/jpa/VotoDaoJpaTest.java
+++ src/test/java/es/votaciones/persistence/models/daos/jpa/VotoDaoJpaTest.java
@@ -1,9 +1,17 @@
 package es.votaciones.persistence.models.daos.jpa;
 
+import org.junit.Before;
+
 import es.votaciones.persistence.models.daos.VotoDao;
 import es.votaciones.persistence.models.daos.jpa.data.VotoDaoJpaTestData;
 
 public class VotoDaoJpaTest {
-   private VotoDao dao=null;
+   private VotoDao dao=DaoJpaFactory.getFactory().getVotoDao();
    private VotoDaoJpaTestData data;
+   
+   @Before
+   public void init(){
+	   
+	   data = new VotoDaoJpaTestData();
+ }
 }
---
+++ src/test/java/es/votaciones/persistence/models/daos/jpa/data/VotoDaoJpaTestData.java
@@ -0,0 +1,5 @@
+package es.votaciones.persistence.models.daos.jpa.data;
+
+public class VotoDaoJpaTestData {
+
+}
