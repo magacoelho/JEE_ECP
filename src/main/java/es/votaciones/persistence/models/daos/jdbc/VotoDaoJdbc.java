1281d156bc4a2d8f4e2b0c1f9d73f557602e2d1f
magacoelho <mgacoelho@yahoo.es>
2015-03-09 22:02:26 +0100

    UPDATE VotoDaoJdbc.sqlToCreateTable()

--- src/main/java/es/votaciones/persistence/models/daos/jdbc/VotoDaoJdbc.java
+++ src/main/java/es/votaciones/persistence/models/daos/jdbc/VotoDaoJdbc.java
@@ -3,6 +3,7 @@ package es.votaciones.persistence.models.daos.jdbc;
 import java.util.List;
 
 import es.votaciones.persistence.models.daos.VotoDao;
+import es.votaciones.persistence.models.entities.Tema;
 import es.votaciones.persistence.models.entities.Voto;
 
 public class VotoDaoJdbc extends GenericDaoJdbc<Voto, Integer> implements VotoDao{
@@ -36,10 +37,14 @@ public class VotoDaoJdbc extends GenericDaoJdbc<Voto, Integer> implements VotoDa
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	 private static final String SQL_CREATE_TABLE = "CREATE TABLE %s (%s INT NOT NULL AUTO_INCREMENT, %s INT, "
+	            + "%s VARCHAR(255), %s VARCHAR(255), %s INT, PRIMARY KEY (%s), "
+	            + "FOREIGN KEY(%s) REFERENCES %s(ID) )";
 	public static String sqlToCreateTable() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return String.format(SQL_CREATE_TABLE, Voto.TABLE, Voto.ID, 
+				Voto.VALORACION, Voto.NIVEL_ESTUDIO, Voto.IP, Voto.TEMA_ID, Voto.ID,
+				Voto.TEMA_ID, Tema.TABLE);
 	}
 
 }
