2e0232b58059adfc3bd0b62f76e38c235729b73f
magacoelho <mgacoelho@yahoo.es>
2015-03-17 01:17:25 +0100

    UPDATE Dispatcher.doPost

--- src/main/java/es/votaciones/views/beans/Dispatcher.java
+++ src/main/java/es/votaciones/views/beans/Dispatcher.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.votaciones.persistence.models.entities.Tema;
+
 
 @WebServlet("/jsp/*")
 public class Dispatcher extends HttpServlet {
@@ -54,7 +56,42 @@ public class Dispatcher extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getPathInfo().substring(1);
+		String view;
+		switch(action){
 		
+			case "votar":       VotarViewBean votarView = new VotarViewBean();
+						       
+						        request.setAttribute(action,votarView);
+						        view=action;
+								break;
+			
+			case "agregar":     Tema  tema = new Tema();
+			                    tema.setDescripcion(request.getParameter("descripcion"));
+			                    tema.setPregunta(request.getParameter("pregunta"));
+				                AgregarViewBean agregarViewBean = new AgregarViewBean();
+						        agregarViewBean.setTema(tema);
+						        request.setAttribute(action,agregarViewBean);
+								view=  agregarViewBean.process();
+								break;
+						
+			case "verVotaciones":VerVotacionesViewBean verVotacionesView = new  VerVotacionesViewBean();
+						        
+						        request.setAttribute(action,verVotacionesView);
+								view=action;
+								break;
+								
+			case "eliminar":    EliminarViewBean eliminarView = new EliminarViewBean();
+	        
+								request.setAttribute(action,eliminarView );
+								view=action;
+								break;
+							
+			default:
+							    view="home";
+							    break;
+			}
+		this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW+ view +"View.jsp").forward(request, response);
 	}
 
 }
