5f00d42f11a998ae4a2b03e33eb696b170c4cdae
magacoelho <mgacoelho@yahoo.es>
2015-03-17 01:06:33 +0100

    UPDATE AgregarVotoViewBean.update, process.. copy de roles ADD atributs

--- src/main/java/es/votaciones/views/beans/AgregarViewBean.java
+++ src/main/java/es/votaciones/views/beans/AgregarViewBean.java
@@ -1,5 +1,41 @@
 package es.votaciones.views.beans;
 
+import org.apache.logging.log4j.LogManager;
+
+
+import es.votaciones.persistence.models.entities.Tema;
+
 public class AgregarViewBean {
+	
+	private Tema tema;
+	
+	
+	public AgregarViewBean() {
+		
+	}
+
+
+	public Tema getTema() {
+		return tema;
+	}
+
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	public void update() {
+        LogManager.getLogger(AgregarViewBean.class).debug(
+                "Se accede a la capa de negocio para recuperar roles");
+        this.tema = new Tema();//roles = new String[] {"uno", "dos", "tres"};
+    }
+
+    public String process() {
+        LogManager.getLogger(AgregarViewBean.class).debug(
+                "Se accede a la capa de negocio para registrar rol: " + tema);
+        return "home";
+    }
+	
+	
 
 }
