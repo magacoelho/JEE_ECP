db9dc019e0680503c052560204c007f2d8dc9bb0
magacoelho <mgacoelho@yahoo.es>
2015-03-20 19:50:35 +0100

    UPDTAE EliminarTemaEjbController.eliminar()

--- src/main/java/es/votaciones/controllers/ejb/EliminarTemaEjbController.java
+++ src/main/java/es/votaciones/controllers/ejb/EliminarTemaEjbController.java
@@ -1,18 +1,15 @@
 package es.votaciones.controllers.ejb;
 
-import java.util.List;
+
 
 import es.votaciones.controllers.EliminarTemaController;
+import es.votaciones.persistence.models.daos.DaoFactory;
 import es.votaciones.persistence.models.entities.Tema;
-import es.votaciones.persistence.models.utils.NivelEstudio;
+
 
 public class EliminarTemaEjbController extends ListarTodosEjbController implements EliminarTemaController {
 	@Override
 	public void eliminar(Tema tema) {
-		// TODO Auto-generated method stub
-		
+		DaoFactory.getFactory().getVotoDao().deleteById(tema.getId());
 	}
-
-	
-
 }
