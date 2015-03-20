2927ab8d10d5bb99e3ac43d0a7e654fa95425871
magacoelho <mgacoelho@yahoo.es>
2015-03-20 21:17:50 +0100

    UPDATE EliminarViewBean.update, process.. copy refactor..getter y setters

--- src/main/java/es/votaciones/views/beans/EliminarViewBean.java
+++ src/main/java/es/votaciones/views/beans/EliminarViewBean.java
@@ -2,12 +2,58 @@ package es.votaciones.views.beans;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+
+import es.votaciones.controllers.ControllerFactory;
+import es.votaciones.controllers.EliminarTemaController;
 import es.votaciones.persistence.models.entities.Tema;
 
 public class EliminarViewBean extends ViewBean {
 	 private List<Tema> temas;
 	 private String mensaje;
 	 private String claveAcceso;
+	 private Integer idTema;
+	 
+	public Integer getIdTema() {
+		return idTema;
+	}
+	public void setIdTema(Integer idTema) {
+		this.idTema = idTema;
+	}
+	public List<Tema> getTemas() {
+		return temas;
+	}
+	public void setTemas(List<Tema> temas) {
+		this.temas = temas;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public String getClaveAcceso() {
+		return claveAcceso;
+	}
+	public void setClaveAcceso(String claveAcceso) {
+		this.claveAcceso = claveAcceso;
+	}
+	 
+	public void update() {
+        LogManager.getLogger(EliminarViewBean.class).debug(
+                "Se accede a la capa de negocio para recuperar Temas");
+        this.temas = temas;//roles = new String[] {"uno", "dos", "tres"};
+    }
+
+    public String process() {
+        LogManager.getLogger(EliminarViewBean.class).debug(
+                "Se accede a la capa de negocio para eliminar Tema: " + idTema);
+        EliminarTemaController eliminarTemaController = ControllerFactory.getControllerFactory().getEliminarTemaController();
+        eliminarTemaController.eliminar(idTema);
+       
+        return "home";
+    }
+	 
 	 
 	 
 
