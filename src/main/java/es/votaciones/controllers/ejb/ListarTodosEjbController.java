c3f08a96ba192cf075e84740e5b91387dcd5942c
magacoelho <mgacoelho@yahoo.es>
2015-03-20 19:33:01 +0100

    ADD ListarTodosEjbController

---
+++ src/main/java/es/votaciones/controllers/ejb/ListarTodosEjbController.java
@@ -0,0 +1,23 @@
+package es.votaciones.controllers.ejb;
+
+import java.util.List;
+
+import es.votaciones.controllers.ListarTodosController;
+import es.votaciones.persistence.models.entities.Tema;
+import es.votaciones.persistence.models.utils.NivelEstudio;
+
+public class ListarTodosEjbController implements ListarTodosController{
+
+	@Override
+	public List<Tema> todosTemas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<NivelEstudio> todosNivelesEstudio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
