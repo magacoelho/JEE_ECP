fe6a876beb7efcb80bc9370057521e0924f920d2
magacoelho <mgacoelho@yahoo.es>
2015-03-21 18:27:39 +0100

    INITIAL use Case Ver Votaciones ADD VotosPorTema

---
+++ src/main/java/es/votaciones/persistence/models/utils/VotosPorTema.java
@@ -0,0 +1,12 @@
+package es.votaciones.persistence.models.utils;
+
+import es.votaciones.persistence.models.entities.Tema;
+
+public class VotosPorTema {
+	  private Tema tema;
+	  private Integer suma;
+	  public VotosPorTema() {
+		
+	}
+	  
+}
