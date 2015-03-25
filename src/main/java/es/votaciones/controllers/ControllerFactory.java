b08b1ea4e28e4066ecf8eecb58a998356371a630
bl0819 <bl0819@192.168.7.1>
2015-03-25 18:52:58 +0100

    UPDATE ControllerFactory para poder ahcer injection

--- src/main/java/es/votaciones/controllers/ControllerFactory.java
+++ src/main/java/es/votaciones/controllers/ControllerFactory.java
@@ -5,13 +5,7 @@ package es.votaciones.controllers;
 public abstract class ControllerFactory {
 	public static ControllerFactory controllerFactory=null;
 	
-	public static void setControllerFactory(ControllerFactory controllerFactory) {
-		ControllerFactory.controllerFactory= controllerFactory;
-    }
-    public static ControllerFactory getControllerFactory() {
-    	   assert controllerFactory != null;
-           return controllerFactory;
-      }
+	
 	
 	public abstract VotarController getVotarController();
 	public abstract VerVotacionesController getverVotacionesController();
