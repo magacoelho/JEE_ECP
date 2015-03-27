dd049c687aadc16f8ff232044c5090ab8aea092e
bl0819 <bl0819@192.168.8.2>
2015-03-27 19:32:48 +0100

    UPDATE  i118n-> quitasr atributos y agregar postConstruct

--- src/main/java/es/votaciones/views/beans/I18n.java
+++ src/main/java/es/votaciones/views/beans/I18n.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
@@ -12,32 +13,19 @@ public class I18n {
 
     private Locale locale;
 
-    private Date date = new Date();
-
-    private double number = 0.23;
-
-    private String parameter = "***";
-
+  
     public I18n() {
-        this.locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+       
    }
-
+    @PostConstruct
+    public void inciializa(){
+    	 this.locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
     public Locale getLocale() {
         return this.locale;
     }
 
-    public Date getDate() {
-        return this.date;
-    }
-
-    public double getNumber() {
-        return this.number;
-    }
-
-    public String getParameter() {
-        return parameter;
-    }
-
+   
     public String change(String language) {
         this.locale = new Locale(language);
         FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
